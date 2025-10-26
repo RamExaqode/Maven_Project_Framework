9424e4779826b290279e274029dbb74733f7185e
Ram <ram.marshivane@exaqode.com>
2025-10-26 12:40:24 +0530

    updated tests

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,32 +1,30 @@
 package utils;
 
-import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     private static WebDriver driver;
-    static Logger log = Logger.getLogger(DriverFactory.class);
 
     public static WebDriver getDriver() {
-        String browser = ConfigReader.get("browser");
-        log.info("Requested browser: " + browser);
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--headless"); // Recommended for CI
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--user-data-dir=/tmp/profile-" + System.currentTimeMillis());
 
-        if (browser != null && browser.equalsIgnoreCase("chrome")) {
-            log.info("Launching Chrome browser");
-            driver = new ChromeDriver();
-        } else {
-            log.error("Unsupported or missing browser type in config.properties");
-            throw new RuntimeException("Unsupported browser: " + browser);
+            driver = new ChromeDriver(options);
         }
-
         return driver;
     }
 
     public static void quitDriver() {
         if (driver != null) {
-            log.info("Closing browser");
             driver.quit();
+            driver = null;
         }
     }
 }
