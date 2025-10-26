691e96f6fef73e97f926fda4370959909e81b487
Ram <ram.marshivane@exaqode.com>
2025-10-26 12:43:35 +0530

    Fix ExtentReports initialization for CI

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,50 +1,39 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.*;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import utils.DriverFactory;
-import utils.ExtentManager;
-import utils.ExtentTestManager;
-import utils.ScreenshotUtil;
-
-import java.lang.reflect.Method;
 
 public class BaseTest {
     protected WebDriver driver;
     protected ExtentReports extent;
-
-    @BeforeSuite
-    public void startReport() {
-        extent = ExtentManager.getInstance();
-    }
+    protected ExtentTest test;
 
     @BeforeMethod
-    public void setUp(Method method) {
+    public void setUp() {
+        // Initialize ExtentReports
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/test-output/extentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+
+        // Create a test node with the class name
+        test = extent.createTest(getClass().getSimpleName());
+
+        // Initialize WebDriver
         driver = DriverFactory.getDriver();
-        ExtentTest test = extent.createTest(method.getName());
-        ExtentTestManager.setTest(test);
     }
 
     @AfterMethod
-    public void tearDown(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            String screenshotPath = ScreenshotUtil.captureScreenshot(driver, result.getName());
-            ExtentTestManager.getTest().fail(result.getThrowable());
-            ExtentTestManager.getTest().addScreenCaptureFromPath(screenshotPath);
-        } else if (result.getStatus() == ITestResult.SUCCESS) {
-            ExtentTestManager.getTest().pass("Test passed");
-        } else if (result.getStatus() == ITestResult.SKIP) {
-            ExtentTestManager.getTest().skip("Test skipped");
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
         }
-
-        DriverFactory.quitDriver();
-    }
-
-    @AfterSuite
-    public void flushReport() {
-        extent.flush();
+        if (extent != null) {
+            extent.flush();
+        }
     }
 }
